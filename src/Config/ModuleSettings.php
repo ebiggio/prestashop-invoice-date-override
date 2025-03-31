2e71a99ff94558f9663377057d3e48633cc40803
Enzo Biggio <ebiggio@gmail.com>
2025-03-30 23:27:22 -0300

    Removed unused settings

--- src/Config/ModuleSettings.php
+++ src/Config/ModuleSettings.php
@@ -14,8 +14,6 @@ class ModuleSettings
         'INVOICE_DATE_OVERRIDE_ORDER_STATUS'                             => [],
         'INVOICE_DATE_OVERRIDE_ON_NEW_ORDER'                             => false,
         'INVOICE_DATE_OVERRIDE_ON_STATUS_CHANGE'                         => false,
-        'INVOICE_DATE_OVERRIDE_CLEAR_ON_UNSELECTED_STATUS_CHANGE'        => false,
-        'INVOICE_DATE_OVERRIDE_MANUAL_IGNORE_PREVIOUS_DATE'              => false,
-        'INVOICE_DATE_OVERRIDE_MANUAL_CLEAR_ON_UNSELECTED_STATUS_CHANGE' => false,
+        'INVOICE_DATE_OVERRIDE_CLEAR_ON_UNSELECTED_STATUS_CHANGE'        => false
     ];
 }
